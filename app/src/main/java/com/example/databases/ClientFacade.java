6c6d8dd360cc5325db23a5366b4dee143fc34a9b
Bayan Yahia <142543434+bayanyahia@users.noreply.github.com>
2023-09-11 12:56:31 +0300

    hi

---
+++ app/src/main/java/com/example/databases/ClientFacade.java
@@ -0,0 +1,16 @@
+package com.example.databases;
+
+public abstract class ClientFacade {
+    CompaniesDAO companiesDAO= new CompaniesDBDAO();
+    CustomersDAO customersDAO= new CustomersDBDAO();
+    CouponsDAO couponsDAO= new CouponsDBDAO();
+    public boolean login(String email,String password){
+        String adminEmail = "admin@admin.com";
+        String adminPassword = "admin";
+        if (email.equals(adminEmail)&&password.equals(adminPassword)
+                ||(customersDAO.isCustomerExists(email, password))
+                ||companiesDAO.isCompanyExists(email,password))
+        return true;
+        else return false;
+    }
+}
